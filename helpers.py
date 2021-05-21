37d6d7ba70f22bc1432db97ae2f47bef352d48cb
Sena Cetin <senacetin96@gmail.com>
2021-05-21 03:07:24 +0300

    Updated hyperparams and added separate test funcs

--- helpers.py
+++ helpers.py
@@ -37,8 +37,10 @@ def test(first_utterances):
             TP_s += 1
         elif ((first_utterances.iloc[i]['ground_truth_satisfaction'] == 0) & (first_utterances.iloc[i]['predicted_satisfaction'] == 1)).all():
             FP_s += 1
+            print("False positive satisfaction", first_utterances.iloc[i]['conversation_id'])
         elif ((first_utterances.iloc[i]['ground_truth_satisfaction'] == 1) & (first_utterances.iloc[i]['predicted_satisfaction'] == 0)).all():
             FN_s += 1
+            print("False negative satisfaction", first_utterances.iloc[i]['conversation_id'])
         else:
             TN_s += 1
 
@@ -57,4 +59,41 @@ def test(first_utterances):
                 
     return P_s, R_s, f1_s, acc_s, P_e, R_e, f1_e, acc_e
         
+    
+def test_satisfaction(first_utterances):
+    TP_s = TN_s = FP_s = FN_s = 0
+
+    # Compare satisfaction predictions with ground truths 
+    for i in range(0,len(first_utterances)):
+        if ((first_utterances.iloc[i]['ground_truth_satisfaction'] == 1) & (first_utterances.iloc[i]['predicted_satisfaction'] == 1)).all():
+            TP_s += 1
+        elif ((first_utterances.iloc[i]['ground_truth_satisfaction'] == 0) & (first_utterances.iloc[i]['predicted_satisfaction'] == 1)).all():
+            FP_s += 1
+        elif ((first_utterances.iloc[i]['ground_truth_satisfaction'] == 1) & (first_utterances.iloc[i]['predicted_satisfaction'] == 0)).all():
+            FN_s += 1
+        else:
+            TN_s += 1
+
+    P_s, R_s, f1_s, acc_s = performance(TP_s, FP_s, TN_s, FN_s)
+                
+    return P_s, R_s, f1_s, acc_s
      
+
+def test_engagement(first_utterances):
+    TP_e = TN_e = FP_e = FN_e = 0
+
+    # Compare satisfaction predictions with ground truths 
+    for i in range(0,len(first_utterances)):
+        # Compare engagement predictions with ground truths 
+        if ((first_utterances.iloc[i]['ground_truth_engagement'] == 1) & (first_utterances.iloc[i]['predicted_engagement'] == 1)).all():
+            TP_e += 1
+        elif ((first_utterances.iloc[i]['ground_truth_engagement'] == 0) & (first_utterances.iloc[i]['predicted_engagement'] == 1)).all():
+            FP_e += 1
+        elif ((first_utterances.iloc[i]['ground_truth_engagement'] == 1) & (first_utterances.iloc[i]['predicted_engagement'] == 0)).all():
+            FN_e += 1
+        else:
+            TN_e += 1
+
+    P_e, R_e, f1_e, acc_e = performance(TP_e, FP_e, TN_e, FN_e)
+                
+    return P_e, R_e, f1_e, acc_e
